86a9026cd11ca327a7cd45cd97840620b9a76824
Sandunika Wasala <sandunika.wasala@seer365.com>
2026-05-21 14:40:06 +0530

    when Generating BlobFolder update dataverse

--- QUALIFY-SURVEY-CREATOR-FAPP-UAT/Repository/BlobRepository/BlobRepository.cs
+++ QUALIFY-SURVEY-CREATOR-FAPP-UAT/Repository/BlobRepository/BlobRepository.cs
@@ -1,16 +1,23 @@
 ﻿
 using Azure.Storage.Blobs;
 using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Newtonsoft.Json;
 using QUALIFY_SURVEY_CREATOR_FAPP_UAT.Models.DTOs;
 using System.ComponentModel;
+using System.Net.Http;
 
 namespace QUALIFY_SURVEY_CREATOR_FAPP_UAT.Repository.BlobRepository
 {
-    public class BlobRepository(BlobServiceClient blobServiceClient, IConfiguration configuration) : IBlobRepository
+    public class BlobRepository(BlobServiceClient blobServiceClient, IConfiguration configuration, ServiceClient serviceClient) : IBlobRepository
     {
         //private readonly BlobContainerClient _containerClient = blobServiceClient.GetBlobContainerClient(configuration.GetValue<string>("AzureBlobContainer"));
         private readonly BlobContainerClient _surveyContainerClient = blobServiceClient.GetBlobContainerClient(configuration.GetValue<string>("AzureBlobSurveyContainer"));
         private readonly BlobContainerClient _surveyTemplateContainerClient = blobServiceClient.GetBlobContainerClient(configuration.GetValue<string>("AzureBlobSurveyTemplateContainer"));
+
+        private readonly ServiceClient _serviceClient = serviceClient;
 
         public Task GenerateBlobFolderFromTemplate(BlobFolderGenerateRequest request)
         {
@@ -25,6 +32,36 @@ namespace QUALIFY_SURVEY_CREATOR_FAPP_UAT.Repository.BlobRepository
                     var targetBlob = _surveyContainerClient.GetBlobClient(targetName);
                     targetBlob.StartCopyFromUri(item.Uri);
                 }
+                string surveyNewOriginUrl = "https://survey-portal-uat-gxchbpcrc4fkbze3.uksouth-01.azurewebsites.net/"+ request.FolderName;
+                string surveyId = "";
+                var getSurveyDetailsFetch = String.Format(@"<fetch top='1'>
+                      <entity name='seer_surveys'>
+                        <attribute name='seer_surveysid' />
+                        <filter>
+                          <condition attribute='seer_neworiginurl' operator='eq' value='{0}'/>
+                        </filter>
+                      </entity>
+                    </fetch>", surveyNewOriginUrl);
+
+                var getSurveyDetails = _serviceClient.RetrieveMultiple(new FetchExpression(getSurveyDetailsFetch));
+
+
+                foreach (var getSurveyDetail in getSurveyDetails.Entities)
+                {
+                    if (getSurveyDetail.Attributes.ContainsKey("seer_surveysid"))
+                    {
+                        surveyId = getSurveyDetail.Attributes["seer_surveysid"].ToString();
+
+                    }
+                }
+
+                // Create an instance of the entity you want to update
+                Entity surveyEntity = new Entity("seer_surveys", new Guid(surveyId));
+
+                surveyEntity["seer_bloburl"] = "https://gyde365qualifysurveyimg.blob.core.windows.net/userfunctions/" + request.FolderName +"/";
+                //Upodate Entity
+                _serviceClient.Update(surveyEntity);
+
                 return Task.CompletedTask; 
             }
             catch (Exception e)
